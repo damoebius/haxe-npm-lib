9c18c9747636e5802a78238216205e5d1ff34230
Jason O'Neil <jason.oneil@gmail.com>
2012-09-25 18:48:04 +0800

    Added licence file to project

--- src/Test.hx
+++ src/Test.hx
@@ -1,3 +1,12 @@
+/****
+* ORIGINAL LIBRARY:
+* Bean - copyright (c) Jacob Thornton 2011-2012
+* https://github.com/fat/bean
+* MIT license
+* 
+* Externs by Jason O'Neil 2012.  No further restrictions other than Jacob Thornton's MIT licence on the original source.
+****/
+
 class Test 
 {
 	static function main()
