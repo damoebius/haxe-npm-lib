10bfc75c969ce387a56447ee7c6527880123c2d4
Jason O'Neil <jason.oneil@gmail.com>
2012-09-25 18:48:16 +0800

    Got rid of old comment in test file

--- test/BeanTest.hx
+++ test/BeanTest.hx
@@ -7,11 +7,6 @@ import js.JQuery;
 import js.Dom.HtmlDom;
 import Bean;
 
-/**
-* Auto generated ExampleTest for MassiveUnit. 
-* This is an example test class can be used as a template for writing normal and async tests 
-* Refer to munit command line tool for more information (haxelib run munit)
-*/
 class BeanTest 
 {
 	private var timer:Timer;
